6daa0fb03109f7ac07c5af54466302e76019a0b6
Your Name <your.email@example.com>
2023-11-15 16:30:43 +0300

    correction

--- path.c
+++ path.c
@@ -7,9 +7,10 @@
 char *_getpath(char *cmd)
 {
 	char *path_env, *fulcmd, *dir;
-	int i = 0;
-	struct stat st;
-
+	/*int i = 0;*/
+	/*int cmd_len = _strlen(cmd);*/
+	/*struct stat st;*/
+/*
 	for (i = 0; cmd[i]; i++)
 	{
 		if (cmd[i] == '/')
@@ -20,12 +21,12 @@ char *_getpath(char *cmd)
 			}
 			return (NULL);
 		}
-	}
+	}*/
 	path_env = _getenv("PATH");
-	if (!path_env)
-		return (NULL);
+	/*if (!path_env)
+		return (NULL);*/
 	dir = strtok(path_env, ":");
-	while (dir)
+	/*while (dir)
 	{
 		fulcmd = malloc(_strlen(dir) + _strlen(cmd) + 2);
 		if (fulcmd)
@@ -45,4 +46,21 @@ char *_getpath(char *cmd)
 	}
 	free(path_env);
 	return (NULL);
+}*/
+	while (dir != NULL)
+	{
+		fulcmd = malloc(_strlen(dir) + _strlen(cmd) + 2);
+		write(*fulcmd, dir, _strlen(dir));
+		write(*fulcmd, "/", 1);
+		write(*fulcmd, cmd, _strlen(cmd));
+	if (access(fulcmd, X_OK) == 0)
+	{
+		return (fulcmd);
+	}
+	free(fulcmd);
+	dir = strtok(NULL, ":");
+	}
+	return (NULL);
 }
+
+
